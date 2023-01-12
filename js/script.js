864db9d0e3261c9e801c7163f66f83b7e9340caf
smruti <srnayak165@gmail.com>
2023-01-12 11:25:28 +0530

    delete feature added

--- js/script.js
+++ js/script.js
@@ -12,21 +12,33 @@ function onSubmit(event) {
     allInput.forEach(input => {
         obj[input.name] = input.value;
     });
-    
+
     allBook.push(obj);
     renderTable(obj);
 }
 
 function renderTable(object) {
     const tr = document.createElement('tr');
+    const td = document.createElement('td');
     const updateButton = document.createElement('button');
     const deleteButton = document.createElement('button');
 
     updateButton.innerText = "Update";
     deleteButton.innerText = "Delete"
 
-    tr.appendChild(updateButton);
-    tr.appendChild(deleteButton);
+    updateButton.setAttribute('data-bookId', object.bookId)
+    deleteButton.setAttribute('data-bookId', object.bookId)
+
+    updateButton.classList.add('button', 'update-button');
+    deleteButton.classList.add('button', 'delete-button');
+
+    updateButton.addEventListener('click', onUpdate);
+    deleteButton.addEventListener('click', onDelete);
+
+    td.appendChild(updateButton);
+    td.appendChild(deleteButton);
+
+    tr.appendChild(td);
 
     for (const data in object) {
         const td = document.createElement('td');
@@ -35,4 +47,19 @@ function renderTable(object) {
     }
 
     tBody.append(tr);
+}
+
+function onUpdate(event) {
+    
+}
+
+function onDelete(event) {
+    if(confirm('Do you want to remove this')) {
+        const deleteButton = event.target;
+        const bookId = deleteButton.dataset.bookId;
+
+        allBook = allBook.filter(book => book.bookId != bookId);
+
+        
+    }   
 }
